48b7beb208ce34a8ebdfb08001eb2299251480dd
Komogorov Denis <komogorov.denis1990@bk.ru>
2024-09-10 03:36:30 +0300

    eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,24 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import stylisticJs from '@stylistic/eslint-plugin-js';
 
 export default [
+  {
+    files: ['**/*.js'],
+    plugins: {
+      stylistic: stylisticJs,
+    },
+    rules: {
+      'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 0 }],
+      'quotes': ['error', 'single'],
+    }
+  },
   pluginJs.configs.recommended,
   {
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node
+        ...globals.node,
       }
     }
   },
@@ -16,8 +27,9 @@ export default [
   },
   {
     rules: {
-      semi: "error",
-      "prefer-const": "error",
+      semi: 'error',
+      'prefer-const': 'error',
+      'arrow-body-style': ['error', 'as-needed'],
     }
   }
 ];
